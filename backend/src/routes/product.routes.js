9b6b8e0364514f71650a1cf2809fea1553d0f651
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-09-24 21:59:24 +0530

    multer for files upload using form data

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,8 +1,39 @@
-import express from "express"
-import { createProductValidator } from "../validators/product.validator"
+import express from "express";
+import { createProductValidator } from "../validators/product.validator.js";
 
-const router = express.Router()
+import multer from "multer";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { createProduct } from "../controllers/product.controller.js";
 
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    files: 5,
+    fileSize: 1 * 1024 * 1024, // 1MB
+  },
+});
 
+const router = express.Router();
 
-export default router
+router.post(
+  "/",
+  authenticate,
+  (req, res, next) => {
+    if (req.user.role !== "seller") {
+      return res.status(403).json({
+        message: "user is not authorize to create products",
+      });
+    }
+    next();
+  },
+  upload.array("images"),
+  (req, res, next) => {
+    req.body?.price && (req.body.price = JSON.parse(req.body.price));
+    req.body?.sizes && (req.body.sizes = JSON.parse(req.body.sizes));
+    next();
+  },
+  createProductValidator,
+  createProduct,
+);
+
+export default router;
