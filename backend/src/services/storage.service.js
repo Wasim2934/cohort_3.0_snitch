b11f4abce25c329939eb199d1c8d288c4e0b82f2
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-09-26 03:24:20 +0530

    style: format code for consistency in storage.service.js

--- backend/src/services/storage.service.js
+++ backend/src/services/storage.service.js
@@ -1,16 +1,16 @@
-import ImageKit, { toFile } from '@imagekit/nodejs';
-import { config } from '../config/config.js';
+import ImageKit, { toFile } from "@imagekit/nodejs";
+import { config } from "../config/config.js";
 
 const client = new ImageKit({
-    privateKey: config.IMAGEKIT_PRIVATE_KEY
+  privateKey: config.IMAGEKIT_PRIVATE_KEY,
 });
 
 export async function uploadFile({ buffer, fileName }) {
-    const response = await client.files.upload({
-        file: await toFile(buffer),
-        fileName: fileName,
-        folder: "snitch"
-    })
+  const response = await client.files.upload({
+    file: await toFile(buffer),
+    fileName: fileName,
+    folder: "snitch",
+  });
 
-    return response;
+  return response;
 }
